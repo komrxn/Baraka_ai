b762fc6da009b99c342fdcd1b4c94592dab0a7dc
komrxn <sultankomronhan@gmail.com>
2025-12-16 01:58:33 +0500

    fix: add base path for frontend assets

--- finance-tracker/vite.config.ts
+++ finance-tracker/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: process.env.BASE_URL || '/',
     plugins: [
       vue(),
       {
